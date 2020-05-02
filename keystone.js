8e8165299dfbb0bf3e5fbbd943a080e7fd4b0226
Juanfran Soler Castaño <juanfran@tiempo.com>
2020-05-02 20:33:37 +0200

    Estructura keystone

---
+++ keystone.js
@@ -0,0 +1,41 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+// Require keystone
+var keystone = require('keystone');
+
+keystone.init({
+	'name': 'covid19',
+	'brand': 'covid19',
+
+	'sass': 'public',
+	'static': 'public',
+	'favicon': 'public/favicon.ico',
+	'views': 'templates/views',
+	'view engine': 'pug',
+
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'Covid19',
+});
+keystone.import('models');
+keystone.set('locals', {
+	_: require('lodash'),
+	env: keystone.get('env'),
+	utils: keystone.utils,
+	editable: keystone.content.editable,
+});
+keystone.set('routes', require('./routes'));
+
+keystone.set('nav', {
+	posts: ['posts', 'post-categories'],
+	galleries: 'galleries',
+	enquiries: 'enquiries',
+	'covid-19s': 'covid-19s',
+});
+
+
+
+keystone.start();
